18364e7819cceb2741281c2d63ef42d47b8a12f8
Leon Weber <leon@leonweber.de>
2013-11-24 14:46:15 +0100

    Add docstring

--- knuth_srng.py
+++ knuth_srng.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 
+""" An implementation of Donald E. Knuth’s “‘Super-random’
+    number generator” given as an educational example of a bad
+    pseudo random number generator in chapter 3.1 of his “The Art of
+    Computer programming” series of books.
+
+    The input value 6065038420 is a fixed point of the algorithm. For
+    other input values, the algorithm has a cycle length of around 3000,
+    making it a pretty pad random number generator.
+
+    Knuth summarized these properties by concluding “The moral of this
+    story is that random numbers should not be generated with a method
+    chosen at random. Some theory should be used.”
+"""
+
 import sys
 
 class KnuthSRNG:
